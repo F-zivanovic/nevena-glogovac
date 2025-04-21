3aaa5f1ae46d84697d017586ade5d02a61b64aab
F-zivanovic <filip.zivanovic1999gmail.com>
2025-04-21 09:54:18 +0200

    we are close

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,48 @@
-// Open/close mobile menu
+const nav = document.getElementById("nav");
 const hamburgerIcon = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("nav-list");
+const mobileMenuLinks = document.querySelectorAll(".nav__link");
 
+/**
+ * Open/Close mobile menu when click on hambuger icon (hamburgerIcon) 
+ */
 hamburgerIcon.addEventListener("click", () => { mobileMenu.classList.toggle("active"); });
 
-// Close mobile menu when click on someone link
-let mobileMenuLinks = document.querySelectorAll(".nav__link");
+
+/**
+ * Close mobiloe menu (mobileMenu) wehn click on wehteever link inside them
+ */
 mobileMenuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
   });
 });
 
-// Close mobile menu when user screen > 1200px
+
+/**
+ * Close mobile menu when user screen > 1200px  
+ * */ 
 window.addEventListener("resize", () => {
   if (window.innerWidth > 1200) {
     mobileMenu.classList.remove("active");
   }
 });
 
-// FAQ section
+/**
+ * Fixing nav (nav) section when user scrol higher than 200px
+ * */
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
+
+/**
+ * Expand FAQ cards on clik
+ */
 const faqItems = document.querySelectorAll(".faq__item");
 
 faqItems.forEach((item) => {
@@ -45,7 +68,11 @@ faqItems.forEach((item) => {
   });
 });
 
-// Arrow up icon
+
+/**
+ * Show icon (arrow__up) wehn user scroll is higher than 100px
+ * Back to top when user click on arrow (arrow__up)
+ */
 const arrowUp = document.querySelector(".arrow__up");
 
 document.addEventListener("scroll", () => {
@@ -60,7 +87,10 @@ arrowUp.addEventListener("click", () => {
   window.scroll({ top: 0, behavior: "smooth" });
 });
 
-// Testimonial section
+
+/**
+ * Testimonial slider, changing images
+ */
 const slides = document.querySelector(".testimonial__slides");
 const testimonials = document.querySelectorAll(".testimonial__item");
 const prevButton = document.getElementById("prev");
